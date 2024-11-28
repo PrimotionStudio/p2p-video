897bdc2067973328085a5ee25db2c0aa7ac47abb
The Primotion Studio <oyedelenewton@gmail.com>
2024-11-28 21:26:31 +0100

    "Added peer connection and WebRTC functionality to main.js"

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 let localStream;
 let remoteStream;
+let peerConnection;
+
+const servers = {
+	iceServers: [
+		{
+			urls: [
+				'stun:stun.l.google.com:3478',
+				'stun:stun.l.google.com:19302',
+				'stun:stun.l.google.com:5349',
+				'stun:stun1.l.google.com:3478',
+				'stun:stun1.l.google.com:5349',
+				'stun:stun2.l.google.com:19302',
+				'stun:stun2.l.google.com:5349',
+				'stun:stun3.l.google.com:3478',
+				'stun:stun3.l.google.com:5349',
+				'stun:stun4.l.google.com:19302',
+				'stun:stun4.l.google.com:5349',
+			],
+		},
+	],
+};
 
 const init = async () => {
 	localStream = await navigator.mediaDevices.getUserMedia({
@@ -7,5 +28,35 @@ const init = async () => {
 		video: true,
 	});
 	document.getElementById('user-1').srcObject = localStream;
+
+	createOffer();
+};
+
+const createOffer = async () => {
+	peerConnection = new RTCPeerConnection(servers);
+
+	remoteStream = new MediaStream();
+	document.getElementById('user-2').srcObject = remoteStream;
+
+	localStream.getTracks().forEach((track) => {
+		peerConnection.addTrack(track, localStream);
+	});
+
+	peerConnection.ontrack = (event) => {
+		event.streams[0].getTracks().forEach((track) => {
+			remoteStream.addTrack(track);
+		});
+	};
+
+	peerConnection.onicecandidate = async (event) => {
+		if (event.candidate) {
+			console.log('Candidate:', event.candidate);
+		}
+	};
+
+	const offer = await peerConnection.createOffer();
+	await peerConnection.setLocalDescription(offer);
+
+	console.log('Offer:', offer);
 };
 init();
